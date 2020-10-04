706b186728842600e7fedfaabea7c74c8d3c5306
Неля <nelly.turak@gmail.com>
2020-10-04 22:04:30 +0300

    Учебный проект: холст

--- js/stat.js
+++ js/stat.js
@@ -1,23 +1,5 @@
 'use strict';
 
-<<<<<<< Updated upstream
-var CLOUD_WIDTH = 420;
-var CLOUD_HEIGHT = 270;
-var CLOUD_X = 100;
-var CLOUD_Y = 50;
-var GAP = 10;
-var FONT_GAP = 15;
-var TEXT_WIDTH = 50;
-var BAR_HEIGHT = 20;
-var barWidth = CLOUD_WIDTH - GAP - TEXT_WIDTH - GAP;
-
-ctx.font = '16px PT Mono';
-ctx.textBaseline = 'hanging';
-ctx.fillText('Ура вы победили!', 0, 10);
-ctx.fillText('Список результатов:', 0, 48);
-
-var renderCloud = function(ctx, x, y, color) {
-=======
 const CLOUD_WIDTH = 400;
 const CLOUD_HEIGHT = 270;
 const CLOUD_X = 100;
@@ -33,22 +15,14 @@ const PLAYER_COLOR = `rgba(255, 0, 0, 1)`;
 
 
 const renderCloud = function (ctx, x, y, color) {
->>>>>>> Stashed changes
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-<<<<<<< Updated upstream
-var getMaxElement = function(arr) {
-  var maxElement = arr[0];
-
-  for (var i = 1; i < arr.length; i++) {
-=======
 const getMaxElement = function (arr) {
   let maxElement = arr[0];
 
   for (let i = 1; i < arr.length; i++) {
->>>>>>> Stashed changes
     if (arr[i] > maxElement) {
       maxElement = arr[i];
     }
@@ -57,37 +31,6 @@ const getMaxElement = function (arr) {
   return maxElement;
 };
 
-<<<<<<< Updated upstream
-window.renderStatistics = function(ctx, names, times) {
-  renderCloud(
-    ctx,
-    CLOUD_X + GAP,
-    CLOUD_Y + GAP,
-    'rgba(0, 0, 0, 0.7)'
-  );
-  renderCloud(
-    ctx,
-    CLOUD_X,
-    CLOUD_Y,
-    '#fff'
-  );
-
-  ctx.fillStyle = 'red';
-
-  var maxTime = getMaxElement(times);
-
-  for (var i = 0; i < names.length; i++) {
-    ctx.fillText(
-      names[i],
-      CLOUD_X + GAP,
-      CLOUD_Y + GAP + FONT_GAP + (GAP + BAR_HEIGHT) * i
-    );
-    ctx.fillRect(
-      CLOUD_X + GAP + TEXT_WIDTH,
-      CLOUD_Y + GAP + (GAP + BAR_HEIGHT) * i,
-      (barWidth * times[i]) / maxTime,
-      BAR_HEIGHT
-=======
 window.renderStatistics = function (ctx, names, times) {
   renderCloud(
       ctx,
@@ -142,7 +85,6 @@ window.renderStatistics = function (ctx, names, times) {
         CLOUD_Y + 9 * SHIFT + (COLUMN_HEIGHT * (1 - times[i] / maxTime)),
         COLUMN_WIDTH,
         COLUMN_HEIGHT * times[i] / maxTime - SHIFT
->>>>>>> Stashed changes
     );
   }
 };
